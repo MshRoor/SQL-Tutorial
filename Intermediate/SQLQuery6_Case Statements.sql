e7bac1ab5d43e954e352807ec8bdfd87d93b2db1
masroor-rahman <mashroor.n7@gmail.com>
2023-02-19 20:03:13 +0600

    made changes to Case statements file

--- Intermediate/SQLQuery6_Case Statements.sql
+++ Intermediate/SQLQuery6_Case Statements.sql
@@ -1,4 +1,22 @@
-select FirstName, LastName, Age
+--select FirstName, LastName, Age,
+--Case
+--	When Age > 30 Then 'Old'
+--	When Age Between 27 And 30 Then 'Young'
+--	Else 'Baby'
+--End
+--from SQLTutorial.dbo.EmployeeDemographics
+--where Age Is Not Null
+--order by Age
+
+Select FirstName, LastName, JobTitle, Salary,
+
+Case
+	When Jobtitle = 'Salesman' Then Salary + (Salary * .10)
+	When Jobtitle = 'Accountant' then Salary + (Salary * .05)
+	When Jobtitle = 'HR' Then Salary + (Salary * 0.000001)
+	Else Salary + (Salary * 0.03)
+End As SalaryAfterRaise
+
 from SQLTutorial.dbo.EmployeeDemographics
-where Age Is Not Null
-order by Age
+Join SQLTutorial.dbo.EmployeeSalary
+	on EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
